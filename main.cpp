32b73f7e9e26d8789a1cdf8c1f056c5642b97155
Oktawian Rekawek <rekawek.oktawian@gmail.com>
2019-12-10 11:04:45 +0100

    Add registrered user to vector

--- main.cpp
+++ main.cpp
@@ -4,8 +4,12 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     KsiazkaAdresowa ksiazkaAdresowa;
+
+    ksiazkaAdresowa.rejestracjaUzytkownika();
+    ksiazkaAdresowa.rejestracjaUzytkownika();
+    ksiazkaAdresowa.rejestracjaUzytkownika();
+    ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
     return 0;
 }
