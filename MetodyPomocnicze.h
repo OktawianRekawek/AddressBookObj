c16d14224042d7b817f227a89bad0a0bfa2d0d7b
Oktawian Rekawek <rekawek.oktawian@gmail.com>
2019-12-11 12:24:35 +0100

    Devide KsiazkaAdresowa to smaller class

---
+++ MetodyPomocnicze.h
@@ -0,0 +1,19 @@
+#ifndef METODYPOMOCNICZE_H
+#define METODYPOMOCNICZE_H
+
+
+#include <iostream>
+#include <windows.h>
+#include <sstream>
+
+#include "Uzytkownik.h"
+
+using namespace std;
+
+class MetodyPomocnicze {
+
+public:
+       static string konwerjsaIntNaString(int);
+};
+
+#endif // METODYPOMOCNICZE_H
