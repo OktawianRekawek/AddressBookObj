a07b2fb1209d0d9c1c7cc720d4f73682f950d820
Oktawian Rekawek <rekawek.oktawian@gmail.com>
2019-12-19 18:33:17 +0100

    Added menu

--- main.cpp
+++ main.cpp
@@ -1,20 +1,104 @@
 #include <iostream>
 
 #include "KsiazkaAdresowa.h"
+#include "MetodyPomocnicze.h"
 
 using namespace std;
 
+char wybierzOpcjeZMenuGlownego() {
+    char wybor;
+
+    system("cls");
+    cout << "    >>> MENU  GLOWNE <<<" << endl;
+    cout << "---------------------------" << endl;
+    cout << "1. Rejestracja" << endl;
+    cout << "2. Logowanie" << endl;
+    cout << "9. Koniec programu" << endl;
+    cout << "---------------------------" << endl;
+    cout << "Twoj wybor: ";
+    wybor = MetodyPomocnicze::wczytajZnak();
+
+    return wybor;
+}
+
+char wybierzOpcjeZMenuUzytkownika() {
+    char wybor;
+
+    system("cls");
+    cout << " >>> MENU UZYTKOWNIKA <<<" << endl;
+    cout << "---------------------------" << endl;
+    cout << "1. Dodaj adresata" << endl;
+    cout << "2. Wyszukaj po imieniu" << endl;
+    cout << "3. Wyszukaj po nazwisku" << endl;
+    cout << "4. Wyswietl adresatow" << endl;
+    cout << "5. Usun adresata" << endl;
+    cout << "6. Edytuj adresata" << endl;
+    cout << "---------------------------" << endl;
+    cout << "7. Zmien haslo" << endl;
+    cout << "8. Wyloguj sie" << endl;
+    cout << "---------------------------" << endl;
+    cout << "Twoj wybor: ";
+    wybor = MetodyPomocnicze::wczytajZnak();
+
+    return wybor;
+}
+
 int main() {
     KsiazkaAdresowa ksiazkaAdresowa("Uzytkownicy.txt", "Adresaci.txt");
+    char wybor;
 
-    ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
-    ksiazkaAdresowa.rejestracjaUzytkownika();
-    ksiazkaAdresowa.logowanieUzytkownika();
-    ksiazkaAdresowa.wypiszWszystkichAdresatow();
-    ksiazkaAdresowa.dodajAdresata();
-    ksiazkaAdresowa.dodajAdresata();
-    ksiazkaAdresowa.wypiszWszystkichAdresatow();
-    ksiazkaAdresowa.wylogowanieUzytownika();
-    ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
+    while (true) {
+        if (ksiazkaAdresowa.czyUzytkownikJestZalogowany()) {
+            wybor = wybierzOpcjeZMenuUzytkownika();
+
+            switch (wybor) {
+            case '1':
+                ksiazkaAdresowa.dodajAdresata();
+                break;
+            case '2':
+                //wyszukiwanie po imieniu
+                break;
+            case '3':
+                //wyszukiwanie po nazwisku
+                break;
+            case '4':
+                ksiazkaAdresowa.wypiszWszystkichAdresatow();
+                break;
+            case '5':
+                //idUsunietegoAdresata = usunAdresata(adresaci);
+                //idOstatniegoAdresata = podajIdOstatniegoAdresataPoUsunieciuWybranegoAdresata(idUsunietegoAdresata, idOstatniegoAdresata);
+                break;
+            case '6':
+                //edytujAdresata(adresaci);
+                break;
+            case '7':
+                ksiazkaAdresowa.zmianaHaslaZalogowanegoUzytkownika();
+                //zmianaHaslaZalogowanegoUzytkownika(uzytkownicy, idZalogowanegoUzytkownika);
+                break;
+            case '8':
+                ksiazkaAdresowa.wylogowanieUzytownika();
+                break;
+            }
+        } else {
+            wybor = wybierzOpcjeZMenuGlownego();
+
+            switch (wybor) {
+            case '1':
+                ksiazkaAdresowa.rejestracjaUzytkownika();
+                break;
+            case '2':
+                ksiazkaAdresowa.logowanieUzytkownika();
+                break;
+            case '9':
+                exit(0);
+                break;
+            default:
+                cout << endl << "Nie ma takiej opcji w menu." << endl << endl;
+                system("pause");
+                break;
+            }
+
+        }
+    }
     return 0;
 }
